afef2cc918690672ccabf832b110b3a2e9ffa4ec
Mario <soymarioyo@gmail.com>
2024-05-01 14:24:13 +0200

    Revert "Organización proyecto y creación de fragmento bascula"
    This reverts commit 73c74619ac277eb82b7605ba3c78a07b0ed16b57.

--- FormaFit/app/src/main/java/com/example/formafit/MainActivity.java
+++ FormaFit/app/src/main/java/com/example/formafit/MainActivity.java
@@ -1,34 +1,24 @@
-package com.example.formafit.activities;
+package com.example.formafit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.formafit.R;
-import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
+    TextView textCartItemCount;
+    int mCartItemCount = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        // Cambiar la barra de color de arriba en negro
-        getWindow().setStatusBarColor(Color.BLACK);
-
 
     }
 }
